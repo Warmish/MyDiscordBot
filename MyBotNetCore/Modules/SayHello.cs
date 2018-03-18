9e8b27551c5ec0191f4d32b7fc08efa55d349fc1
Rafal Parafiniuk <rafx11@hotmail.com>
2018-03-18 11:05:01 +0100

    First Commit

---
+++ MyBotNetCore/Modules/SayHello.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBotNetCore.Modules
+{
+    public class SayHello : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello")]
+        public async Task SayHelloAsync()
+        {
+            await ReplyAsync("Hello in ❤ Warmiland ❤");
+        }
+    }
+}
---
+++ MyBotNetCore/MyBot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+
+namespace MyBotNetCore
+{
+    class MyBot
+    {
+        
+        public MyBot()
+        {
+
+        }
+    }
+}
